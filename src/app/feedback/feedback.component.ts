9a101c50f1483dcd0c8508845b5acbe09241634a
Lemon <lemonhe@tudelft.nl>
2025-05-03 11:38:41 +0200

    Add Basic webpage structure

---
+++ src/app/feedback/feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ModelOutput } from '../ModelOutput';
+
+@Component({
+  selector: 'app-feedback',
+  imports: [],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.css'
+})
+export class FeedbackComponent {
+  @Input() feedback!: ModelOutput;
+  // variable: dataType
+
+}
--- src/app/sentiment-analysis/sentiment-analysis.component.ts
+++ src/app/sentiment-analysis/sentiment-analysis.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
+import { FeedbackComponent } from '../feedback/feedback.component';
+import { ModelOutput } from '../ModelOutput';
 
 @Component({
   selector: 'app-sentiment-analysis',
-  imports: [],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Search Area" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-  `,
+  imports: [FeedbackComponent],
+  templateUrl: './sentiment-analysis.component.html',
   styleUrl: './sentiment-analysis.component.css'
 })
 export class SentimentAnalysisComponent {
-
+  modelOutput: ModelOutput = {
+    analysis: "positive",
+    version: "1.0.0",
+  }
 }
