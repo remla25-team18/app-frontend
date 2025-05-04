c769e6bf95999cdb048e5cd61e8eec0441ff8afa
Lemon <lemonhe@tudelft.nl>
2025-05-04 10:08:20 +0200

    Add Http communication from json file

--- src/app/sentiment-analysis/sentiment-analysis.component.ts
+++ src/app/sentiment-analysis/sentiment-analysis.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { FeedbackComponent } from '../feedback/feedback.component';
 import { ModelOutput } from '../ModelOutput';
 
@@ -8,9 +8,30 @@ import { ModelOutput } from '../ModelOutput';
   templateUrl: './sentiment-analysis.component.html',
   styleUrl: './sentiment-analysis.component.css'
 })
+@Injectable({
+  providedIn: 'root',
+})
 export class SentimentAnalysisComponent {
+  url = "http://localhost:3000/modelOutput";
   modelOutput: ModelOutput = {
-    analysis: "positive",
-    version: "1.0.0",
+     analysis: "positive",
+     version: "1.0.0"
+  };
+  loading: boolean = true;
+
+  async getResponce(): Promise<void> {
+    const data = await fetch(this.url); // TODO: improve with HttpClient
+    const dataList = (await data.json()) ?? null; 
+    console.log(dataList[0]);
+    this.loading = false;
+    if (dataList && dataList.length > 0) {
+      this.modelOutput = dataList[0];
+    } else {
+      console.error('No data received from the model output.');
+    }
+  }
+
+  ngOnInit(): void {
+    this.getResponce();
   }
 }
